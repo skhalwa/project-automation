3bdee224dbe52eedc9982c7c99f62469d6861b4f
shubhamkosta <shubham1994rocks@gmail.com>
2023-05-22 13:59:25 +0530

    commit

--- main.tf
+++ main.tf
@@ -159,11 +159,19 @@ resource "azurerm_virtual_machine" "main" {
 #     ]
 #   }
 # }
+resource "azurerm_snapshot_source" "example" {
+  name                = "example-snapshot-source"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  managed_disk_id     = azurerm_virtual_machine.main.storage_os_disk.0.managed_disk_id
+}
+
 resource "azurerm_snapshot" "example" {
   name                = "example-snapshot"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  source_id           = azurerm_virtual_machine.main.storage_os_disk.0.managed_disk_id
+  source {
+    id = azurerm_snapshot_source.example.id
+  }
 }
-
 
